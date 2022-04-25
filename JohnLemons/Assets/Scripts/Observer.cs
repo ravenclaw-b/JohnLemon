a819634dcc0aebafa0e0ce923bdaca02379290ee
Harry <bamdad.kho@gmail.com>
2022-04-25 18:49:43 +0300

    added unfinished invisibilty script

--- JohnLemons/Assets/Scripts/Observer.cs
+++ JohnLemons/Assets/Scripts/Observer.cs
@@ -2,32 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Observer : MonoBehaviour
+public class Observer : MonoBehaviour 
 {
+    public Invisibilty script;   
+
     public Transform player;
     public GameEnding gameEnding;
 
     bool m_IsPlayerInRange;
 
-    void OnTriggerEnter (Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if (other.transform == player)
+        if(other.transform == player && !script.IsInvisible)
         {
             m_IsPlayerInRange = true;
         }
     }
 
-    void OnTriggerExit (Collider other)
+    void Update()
     {
-        if (other.transform == player)
-        {
-            m_IsPlayerInRange = false;
-        }
-    }
-
-    void Update ()
-    {
-        if (m_IsPlayerInRange)
+        if(m_IsPlayerInRange)
         {
             Vector3 direction = player.position - transform.position + Vector3.up;
             Ray ray = new Ray(transform.position, direction);
@@ -35,7 +29,7 @@ public class Observer : MonoBehaviour
 
             if(Physics.Raycast(ray, out raycastHit))
             {
-                if (raycastHit.collider.transform == player)
+                if(raycastHit.collider.transform == player)
                 {
                     gameEnding.CaughtPlayer();
                 }
