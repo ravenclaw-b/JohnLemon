1d0e312d8d05aaeeb13351a0e7b0e5c37bb36ccd
Harry <bamdad.kho@gmail.com>
2022-06-12 21:13:11 +0300

    finished invisibilty bar, invisibilty feature semi-broken

--- JohnLemons/Assets/Scripts/Invisibilty.cs
+++ JohnLemons/Assets/Scripts/Invisibilty.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
-
 public class Invisibilty : MonoBehaviour
 {
     public bool IsInvisible = false;
     bool IsCooldown = false;
     public GameObject textObject;
 
+    [SerializeField] GameObject bar;
+
     [SerializeField] Material Glass;
     [SerializeField] Color colorInvis;
     [SerializeField] Color colorVis;
+
+    [SerializeField] Color red;
+    [SerializeField] Color green; 
     
 
     void Start()
     {
         Glass.SetColor("_Color", colorVis);
         Glass.SetFloat("_Mode", 0f);
-    }
 
-    void Update() 
-    {
-
+        bar.GetComponent<Image>().color = Color.green;
     }
 
     void FixedUpdate() 
@@ -42,26 +43,36 @@ public class Invisibilty : MonoBehaviour
         Glass.SetColor("_Color", colorInvis);
         Glass.SetFloat("_Mode", 3f);
 
-        for (int i = 5; i >= 1; i--) 
+        for (float i = 5f; i >= 1f; i -= 0.1f) 
         {   
-            textObject.GetComponent<Text>().text = ("Inivisible for: " + i.ToString() + " seconds");
-            yield return new WaitForSeconds(1);
+            textObject.GetComponent<Text>().text = ("Inivisible for: " + Mathf.Round(i) + " seconds");
+            yield return new WaitForSeconds(0.1f);
+
+            bar.GetComponent<RectTransform>().sizeDelta -= new Vector2 (4.9f, 0);
         }
 
         IsInvisible = false;
         IsCooldown = true;
 
         Glass.SetColor("_Color", colorVis);
-        Glass.SetFloat("_Mode", 0f);
+    Glass.SetFloat("_Mode", 0f);
 
         Debug.Log("Invisibilty deactivated");
 
-        for (int i = 10; i >= 1; i--) 
-        {   
-            textObject.GetComponent<Text>().text = ("Cooldown: " + i.ToString() + " seconds");
-            yield return new WaitForSeconds(1);
+        bar.GetComponent<RectTransform>().sizeDelta = new Vector2 (198, 6);
+        bar.GetComponent<Image>().color = Color.red;
+
+        for (float i = 10f; i >= 1; i -= 0.1f) 
+        {  
+            textObject.GetComponent<Text>().text = ("Cooldown: " + Mathf.Round(i) + " seconds");
+            yield return new WaitForSeconds(0.1f);
+
+            bar.GetComponent<RectTransform>().sizeDelta -= new Vector2 (2.2f, 0);
         }
+
         IsCooldown = false;
+        bar.GetComponent<RectTransform>().sizeDelta = new Vector2 (198, 6);
+        bar.GetComponent<Image>().color = Color.green;
         
         textObject.GetComponent<Text>().text = ("Invisibilty Available");
         Debug.Log("Cooldown over");
